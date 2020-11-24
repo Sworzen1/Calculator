0d3e083857e29f7ffbd74821b1c3f9fb8744b404
Bartlomiej Sworzen <b.sworzen@gmail.com>
2020-11-24 22:19:53 +0100

    Updates

---
+++ precache-manifest.57938d73b20c8fc5425d7d7d2159f37e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587b9f7474aba72e3bd1c270a1bff1ac",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "1088b3c32f90c8ddf709",
+    "url": "/calculator/static/css/main.10366f3f.chunk.css"
+  },
+  {
+    "revision": "efc3d230fd1e1b3caa34",
+    "url": "/calculator/static/js/2.bbcd3f81.chunk.js"
+  },
+  {
+    "revision": "1088b3c32f90c8ddf709",
+    "url": "/calculator/static/js/main.91cee391.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
